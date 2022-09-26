13cd3c204d6b5bd16c440e2de7ae536ffd618438
unknown <nehaurkude098@gmail.com>
2022-09-26 16:55:57 +0530

    hibernate codes

---
+++ src/main/java/com/ty/school/dto/Student.java
@@ -0,0 +1,5 @@
+package com.ty.school.dto;
+
+public class Student {
+
+}
---
+++ src/main/java/com/ty/school/dto/User.java
@@ -0,0 +1,54 @@
+package com.ty.school.dto;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "userdetails")
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	@Column(name="username", nullable = false)
+	private String name;
+	@Column(name="phone_no", unique=true)
+	private long phone;
+	private String address;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public long getPhone() {
+		return phone;
+	}
+
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
